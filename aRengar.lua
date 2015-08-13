14d9f5feeb4b1a9eb9911a1038a2a2ea1109b3d1
TheApexx <matt18acw@yahoo.com>
2015-08-12 21:21:43 -0700

    Scripts

--- aRengar.lua
+++ aRengar.lua
@@ -4,10 +4,13 @@ Config = scriptConfig("Rengar", "Apex Rengar")
 Config.addParam("Q", "Use Q At 5 Stacks", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("W", "Use W at 5 stacks", SCRIPT_PARAM_ONOFF, false)
 Config.addParam("E", "Use E at 5 stacks", SCRIPT_PARAM_ONOFF, false)
+Config.addParam("WHeal", "Use W if hp<20%", SCRIPT_PARAM_ONOFF, false)
 Config.addParam("Combo", "Combo", SCRIPT_PARAM_KEYDOWN, string.byte(" "))
 
 OnLoop(function(myHero)
 	local maxmana = GetMaxMana(myHero)
+	local currhp = GetCurrentHP(myHero)
+	local maxhp = GetMaxHP(myHero)
 	local currmana = GetCurrentMana(myHero)
 	local unit = GetTarget(1250, DAMAGE_PHYSICAL)
 	--          numbers=spelltravelspeed, spelldelay, spellrange, spellwidth, collision(t/f), addhitbox(t/f) 
@@ -15,14 +18,30 @@ OnLoop(function(myHero)
 	if Config.Combo then
 		if unit then
 		if currmana == 5 then
-			if CanUseSpell(myHero, _Q) == READY and Config.Q and IsInDistance(unit, 250) then
-			CastSpell(_Q)
+			if Config.WHeal and currhp > .25*maxhp then
+				if CanUseSpell(myHero, _Q) == READY and Config.Q and IsInDistance(unit, 250) then
+				CastSpell(_Q)
+				end
+				if CanUseSpell(myHero, _W) == READY and Config.W and IsInDistance(unit, 500) then
+				CastSpell(_W)
+				end
+				if CanUseSpell(myhero, _E) == READY and Config.E and IsInDistance(unit, 1000) then
+				CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+				end
+			else if Config.WHeal and currhp <= .25*maxhp then
+				CastSpell(_W)
+			else if not Config.Wheal then
+				if CanUseSpell(myHero, _Q) == READY and Config.Q and IsInDistance(unit, 250) then
+				CastSpell(_Q)
+				end
+				if CanUseSpell(myHero, _W) == READY and Config.W and IsInDistance(unit, 500) then
+				CastSpell(_W)
+				end
+				if CanUseSpell(myhero, _E) == READY and Config.E and IsInDistance(unit, 1000) then
+				CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+				end
 			end
-			if CanUseSpell(myHero, _W) == READY and Config.W and IsInDistance(unit, 500) then
-			CastSpell(_W)
 			end
-			if CanUseSpell(myhero, _E) == READY and Config.E and IsInDistance(unit, 1000) then
-			CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 			end
 		end
 		if currmana < 5 then
