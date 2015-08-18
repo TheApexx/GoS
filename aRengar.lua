d67c5e48b432b54f385ce72c22608153697e9628
TheApexx <matt18acw@yahoo.com>
2015-08-17 21:03:57 -0700

    Scripts

--- aRengar.lua
+++ aRengar.lua
@@ -13,53 +13,34 @@ OnLoop(function(myHero)
 	local maxhp = GetMaxHP(myHero)
 	local currmana = GetCurrentMana(myHero)
 	local unit = GetTarget(1250, DAMAGE_PHYSICAL)
-	--          numbers=spelltravelspeed, spelldelay, spellrange, spellwidth, collision(t/f), addhitbox(t/f) 
-	local EPred = GetPredictionForPlayer(GetMyHeroPos(),unit,GetMoveSpeed(unit),900,150,1000,200,true,true)
-	if Config.WHeal and currhp > .25*maxhp then return end
 	if currmana == 5 and Config.WHeal and currhp <= .25*maxhp then
 	CastSpell(_W)
 	end
-	if not Config.WHeal then return end
 	if Config.Combo then
 		if unit then
-		if currmana == 5 then
-			if Config.WHeal and currhp > .25*maxhp then
-				if CanUseSpell(myHero, _Q) == READY and Config.Q and IsInDistance(unit, 250) then
-				CastSpell(_Q)
-				end
-				if CanUseSpell(myHero, _W) == READY and Config.W and IsInDistance(unit, 500) then
-				CastSpell(_W)
-				end
-				if CanUseSpell(myhero, _E) == READY and Config.E and IsInDistance(unit, 1000) then
-				CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
-				end
-			else if Config.WHeal and currhp <= .25*maxhp then
-				CastSpell(_W)
-			else if not Config.Wheal then
-				if CanUseSpell(myHero, _Q) == READY and Config.Q and IsInDistance(unit, 250) then
-				CastSpell(_Q)
-				end
-				if CanUseSpell(myHero, _W) == READY and Config.W and IsInDistance(unit, 500) then
-				CastSpell(_W)
-				end
-				if CanUseSpell(myhero, _E) == READY and Config.E and IsInDistance(unit, 1000) then
-				CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
-				end
-			end
-			end
-			end
-		end
-		if currmana < 5 then
-			if CanUseSpell(myHero, _Q) == READY and IsInDistance(unit, 650) then
+			--          numbers=spelltravelspeed, spelldelay, spellrange, spellwidth, collision(t/f), addhitbox(t/f) 
+			local EPred = GetPredictionForPlayer(GetMyHeroPos(),unit,GetMoveSpeed(unit),900,150,1000,200,true,true)
+			if CanUseSpell(myHero, _Q) == READY and IsInDistance(unit, 550) then
 			CastSpell(_Q)
 			end
 			if CanUseSpell(myHero, _W) == READY and IsInDistance(unit, 500) then
 			CastSpell(_W)
 			end
-			if CanUseSpell(myHero, _E) == READY and IsInDistance(unit, 750) then
+			if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and IsInDistance(unit, 750) then
 			CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 			end
 		end
+		if unit and currmana == 5 then
+			local EPred = GetPredictionForPlayer(GetMyHeroPos(),unit,GetMoveSpeed(unit),900,150,1000,200,true,true)
+			if IsInDistance(unit, 800) and Config.Q then
+			CastSpell(_Q)
+			end
+			if IsInDistance(unit, 500) and Config.W then
+			CastSpell(_W)
+			end
+			if IsInDistance(unit, 750) and EPred.HitChance == 1 and Config.E then
+			CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+			end
 		end
 	end
 end)
